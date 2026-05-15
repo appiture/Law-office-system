a7aa609f371b16c60f827128db45f844560517fe
Lonewolf080910 <roshaanpeeris@gmail.com>
2026-05-15 15:49:45 +0530

    dash

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState, useRef } from "react";
 import { createPortal } from "react-dom";
 import { Link } from "react-router-dom";
 import AppShell from "../components/AppShell";
